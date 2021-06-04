5e68ac8eaabffc9e1c44e3b63bf2a09c4d598054
two <twoartwork@gmail.com>
2021-06-03 20:22:42 -0600

    cat on homepage

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import SvgCat from '../svgComponents/Cat.js'
 
 export default function Home() {
   return (
-    <div>Home</div>
+    <div className="home">
+      <SvgCat />
+    </div>
   )
 }
--- src/svgComponents/index.js
+++ src/svgComponents/index.js
@@ -1,2 +1,3 @@
+export { default as Cat } from './Cat'
 export { default as Logo } from './Logo'
 export { default as Menu } from './Menu'
