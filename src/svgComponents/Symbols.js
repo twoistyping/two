81d67f6284d622d28aab848955900ccff95995d9
two <twoartwork@gmail.com>
2021-06-04 02:01:22 -0600

    fix symbols svg

--- src/svgComponents/Symbols.js
+++ src/svgComponents/Symbols.js
@@ -5,52 +5,40 @@ function SvgSymbols(props) {
     <svg
       id="symbols"
       xmlns="http://www.w3.org/2000/svg"
-      viewBox="0 0 1920 1080"
+      viewBox="0 0 1626 1120"
       {...props}
     >
-      <defs>
-        <clipPath id="symbols_svg__a">
-          <path d="M0 0h1920v1080H0z" />
-        </clipPath>
-      </defs>
-      <g data-name="homepage \u2013 1">
-        <g data-name="Group 68" clipPath="url(#symbols_svg__a)">
-          <g data-name="Group 12" fill="none" stroke="#1e212b">
-            <path data-name="Line 3" d="M192.5 391.5v40" />
-            <path data-name="Line 4" d="M172.5 411.5h40" />
-          </g>
-          <g data-name="Group 45" fill="none" stroke="#1e212b">
-            <path data-name="Line 3" d="M1671.5 1001.5v40" />
-            <path data-name="Line 4" d="M1651.5 1021.5h40" />
-          </g>
-          <g data-name="Group 43">
-            <path
-              data-name="Line 3"
-              fill="none"
-              stroke="#1e212b"
-              d="M1439.5 301.5h40"
-            />
-          </g>
-          <g data-name="Group 43">
-            <path
-              data-name="Line 3"
-              fill="none"
-              stroke="#1e212b"
-              d="M487.5-19.5v40"
-            />
-          </g>
-          <g data-name="Group 14" fill="none" stroke="#1e212b">
-            <path data-name="Line 3" d="M1604.642-14.643l-28.284 28.284" />
-            <path data-name="Line 4" d="M1576.358-14.643l28.284 28.284" />
-          </g>
-          <g data-name="Group 46" fill="none" stroke="#1e212b">
-            <path data-name="Line 3" d="M543.642 906.358l-28.284 28.284" />
-            <path data-name="Line 4" d="M515.358 906.358l28.284 28.284" />
-          </g>
-          <g data-name="Group 47" fill="none" stroke="#1e212b">
-            <path data-name="Line 3" d="M85.5 1060.5v40" />
-            <path data-name="Line 4" d="M65.5 1080.5h40" />
-          </g>
+      <g data-name="Group 68">
+        <g data-name="Group 12" fill="none" stroke="#1e212b">
+          <path data-name="Line 3" d="M127 411v40" />
+          <path data-name="Line 4" d="M107 431h40" />
+        </g>
+        <g data-name="Group 45" fill="none" stroke="#1e212b">
+          <path data-name="Line 3" d="M1606 1021v40" />
+          <path data-name="Line 4" d="M1586 1041h40" />
+        </g>
+        <g data-name="Group 43">
+          <path
+            data-name="Line 3"
+            fill="none"
+            stroke="#1e212b"
+            d="M1374 321h40"
+          />
+        </g>
+        <g data-name="Group 43">
+          <path data-name="Line 3" fill="none" stroke="#1e212b" d="M422 0v40" />
+        </g>
+        <g data-name="Group 14" fill="none" stroke="#1e212b">
+          <path data-name="Line 3" d="M1539.142 4.857l-28.284 28.284" />
+          <path data-name="Line 4" d="M1510.858 4.857l28.284 28.284" />
+        </g>
+        <g data-name="Group 46" fill="none" stroke="#1e212b">
+          <path data-name="Line 3" d="M478.142 925.858l-28.284 28.284" />
+          <path data-name="Line 4" d="M449.858 925.858l28.284 28.284" />
+        </g>
+        <g data-name="Group 47" fill="none" stroke="#1e212b">
+          <path data-name="Line 3" d="M20 1080v40" />
+          <path data-name="Line 4" d="M0 1100h40" />
         </g>
       </g>
     </svg>
