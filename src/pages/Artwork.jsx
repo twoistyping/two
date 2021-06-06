c60c9b2e06a672c1b19cb532fc411d38e21eef3f
two <twoartwork@gmail.com>
2021-06-06 11:18:17 -0600

    don't show content until loaded

--- src/pages/Artwork.jsx
+++ src/pages/Artwork.jsx
@@ -32,22 +32,26 @@ export default function Artwork() {
     <Content type="gallery lines">
       <Loader loading={ loading ? 'show' : 'hide' } />
 
-      <PageLabel label="Artwork" />
-      <GalleryBlurb>
-        <h2>The Gallery</h2>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Et ultrices neque ornare aenean euismod elementum nisi quis. Arcu bibendum at varius vel pharetra vel.</p>
-        <Button text='Twitter' link='https://twitter.com/twoartwork' external='true' />
-        <Button text='Instagram' link='https://instagram.com/twoartwork' style='yellow' external='true' />
-      </GalleryBlurb>
-
-      { 
-        artwork.map(image => (
-          <GalleryItem 
-            image={image.url}
-            title={image.title}
-            alt={image.alt}
-          />
-        ))
+      {
+        loading ? '' :
+        <>
+          <PageLabel label="Artwork" />
+          <GalleryBlurb>
+            <h2>The Gallery</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Et ultrices neque ornare aenean euismod elementum nisi quis. Arcu bibendum at varius vel pharetra vel.</p>
+            <Button text='Twitter' link='https://twitter.com/twoartwork' external='true' />
+            <Button text='Instagram' link='https://instagram.com/twoartwork' style='yellow' external='true' />
+          </GalleryBlurb>
+          {
+            artwork.map(image => (
+              <GalleryItem 
+                image={image.url}
+                title={image.title}
+                alt={image.alt}
+              />
+            ))
+          }
+        </>
       }
       
     </Content>
