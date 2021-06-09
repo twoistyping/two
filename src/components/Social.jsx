5d907ce79add62292edfbdf22d67ee25afd047ed
two <twoartwork@gmail.com>
2021-06-08 23:15:26 -0600

    social links

--- src/components/Social.jsx
+++ src/components/Social.jsx
@@ -6,11 +6,11 @@ import SvgTwitter from '../svgComponents/Twitter.js'
 export default function Social() {
   return (
     <div className="social">
-      <a className='social__single instagram' href='' target='_blank'>
-        <SvgInstagram />
-      </a>
-      <a className='social__single twitter' href='' target='_blank'>
+      <a className='social__single twitter' href='https://twitter.com/twoartwork' target='_blank'>
         <SvgTwitter />
+      </a>
+      <a className='social__single instagram' href='https://instagram.com/twoartwork' target='_blank'>
+        <SvgInstagram />
       </a>
       <a className='social__single email' href='mailto:twoartwork@gmail.com' target='_blank'>twoartwork@gmail.com</a>
     </div>
